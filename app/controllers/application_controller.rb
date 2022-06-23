a9c0cac4c31af3fee0612d2774944c82a433375d
Mai Vang <vmaineng@gmail.com>
2022-06-23 09:13:14 -0500

    created patch for price

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,47 @@ class ApplicationController < Sinatra::Base
   # Add your routes here
 
 #find license plate
-
 post "/vehicles/find_by" do
   license = Vehicle.find_or_create_by(license_plate: params[:vehicle])
   cars = license.parkedcars
   cars.to_json 
 end
 
+#create a new reservation - rough draft
+post "/vehicles" do
+license = Vehicle.create(
+  price: params[:price],
+  arrival: params[:arrival],
+  departure: params[:departure]
+)
+license.to_json
+end
+
+#delete a reservation - rough draft
+delete "/vehicles/:id" do
+  license = Vehicle.find(params[:id])
+license.destroy
+license.to_json
+end
+
+#update a reservation - rough draft
+patch "/parkedcars/:id" do
+  license = Parkedcar.find(params[:id])
+  license.update(
+    price: params[:price]
+  )
+  license.to_json
+end
+
+get "/vehicles" do 
+  data = Vehicle.all.to_json
+ end
+ 
+
 get "/parked_cars" do 
  data = ParkedCar.all.to_json
 end
+
 
   # get "/" do
   #   { message: "Good luck with your project!" }.to_json
--- app/models/parkedcar.rb
+++ app/models/parkedcar.rb
@@ -2,5 +2,9 @@ class Parkedcar < ActiveRecord::Base
     belongs_to :vehicle
     belongs_to :parkinglot
 
+def formatted_time
+    time.strftime("%A, %m/%d/%y %l:%M %p")
+end
+
 
 end
